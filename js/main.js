ce5a0cc9fc5b89a597a8ecfde914b3f2c016f2ea
lucl310 <lucthehuman21@gmail.com>
2017-01-07 09:36:25 -1000

    Finished step 3

--- js/main.js
+++ js/main.js
@@ -1,1 +1,32 @@
 console.log(Phaser);
+//This sets the variable for the spacebar.
+var spaceKey;
+
+var ground;
+var player;
+var obstacle;
+
+//This sets the score to start at -1.
+var score = -1;
+
+
+var GAME_WIDTH = 800;
+var GAME_HEIGHT = 600;
+var GAME_CONTAINER_ID = 'gameDiv';
+
+//This is the object which runs the game.
+function preload(){
+
+},
+
+function create(){
+
+};
+
+function update(){
+
+};
+
+var game = new Phaser.Game(800, 600, Phaser.AUTO, 'gameDiv', { preload: preload, update: update, create: create });
+
+game.state.start();
